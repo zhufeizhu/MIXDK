81a87ee787d0847627bc1ec780a65f9467970462
zhuqs <1426979407@qq.com>
2021-09-13 17:53:37 +0800

    删除不要的文件

--- nvm.h
+++
@@ -1,6 +0,0 @@
-#include "mix_queue.h"
-
-int mix_init_nvm_queue(unsigned int, unsigned int);
-
-
-int mix_post_task_to_nvm(io_task_t*);
--- ssd.h
+++
@@ -1,5 +0,0 @@
-#include "mix_queue.h"
-
-int mix_init_ssd_queue(unsigned int, unsigned int);
-
-int mix_post_task_to_ssd(io_task_t*);
